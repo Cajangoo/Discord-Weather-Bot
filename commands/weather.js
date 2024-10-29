3c42906230cf3733b299306d656dc7be585beeb3
Fernando Cajango Dias <fernandocajango@gmail.com>
2024-10-28 21:52:52 -0300

    feat: commands - test phase

--- commands/weather.js
+++ commands/weather.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fetch = require('node-fetch');
 
 async function getWeather(city) {
-    const apiKey = process.env.OPENWEATHERMAP_API_KEY; // Obtém a chave da API
+    const apiKey = process.env.OPENWEATHER_API_KEY; // Obtém a chave da API
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; // Unidade em Celsius
 
     try {
